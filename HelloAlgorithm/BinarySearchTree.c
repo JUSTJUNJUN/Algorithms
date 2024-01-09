94b04ff137e6335e1636ad98210bf4cf028d0f49
Just Jun <13061980151@163.com>
2024-01-09 23:37:45 +0800

    【入库原因】：新增二叉搜索树例程 【入库说明】：hello算法示例 【入 库 人】：薛豪俊 【日    期】：2024.01.09 【备    注】：无

--- HelloAlgorithm/BinarySearchTree.c
+++ HelloAlgorithm/BinarySearchTree.c
@@ -18,7 +18,7 @@ typedef struct {
 } BinarySearchTree;
 
 
-int
+tree_node *
 find_node_in_tree(BinarySearchTree *bst, int value)
 {
     tree_node *tmp = bst->root;
@@ -34,6 +34,93 @@ find_node_in_tree(BinarySearchTree *bst, int value)
     }
 
     return tmp;
+}
+
+
+void
+insert_node_into_tree(BinarySearchTree *bst, int value)
+{
+    if (!bst->root) {
+        bst->root = (tree_node *)malloc(sizeof(tree_node));
+        bst->root->node_value = value;
+        return;
+    }
+
+    tree_node *cur = bst->root, *pre = NULL;
+    while (cur != NULL) {
+        /* 值为value的节点已存在,直接返回 */
+        if (cur->node_value == value) {
+            return;
+        }
+
+        pre = cur;
+
+        if (cur->node_value < value) {
+            cur = cur->right_node;
+        } else if (cur->node_value > value) {
+            cur = cur->left_node;
+        }
+    }
+    
+    tree_node *new = (tree_node *)malloc(sizeof(tree_node));
+    new->node_value = value;
+
+    if (pre->node_value < value) {
+        pre->right_node = new;
+    } else if (pre->node_value > value) {
+        pre->left_node = new;
+    }
+
+    return;
+}
+
+
+void
+delete_node_off_tree(BinarySearchTree *bst, int value)
+{
+    if (!bst->root) {
+        return;
+    }
+
+    tree_node *cur = bst->root, *pre = NULL;
+
+    while (cur != NULL) {
+        if (cur->node_value == value) {
+            break;
+        }
+        
+        pre = cur;
+
+        if (cur->node_value < value) {
+            cur = cur->right_node;
+        } else if (cur->node_value > value) {
+            cur = cur->left_node;
+        }
+    }
+    
+    if (!cur) {
+        return;
+    }
+
+    if (!cur->left_node || !cur->right_node) {
+        tree_node *child = cur->left_node != NULL ? cur->left_node : cur->right_node;
+        if (pre->left_node == cur) {
+            pre->left_node = child;
+        } else {
+            pre->right_node = child;
+        }
+    } else {
+        tree_node *tmp = cur->right_node;
+        while (tmp->left_node != NULL) {
+            tmp = tmp->left_node;
+        }
+        
+        int tmp_val = tmp->node_value;
+        delete_node_off_tree(bst, tmp_val);
+        cur->node_value = tmp_val;
+    }
+
+    return;
 }
 
 
